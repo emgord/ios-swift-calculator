7d9e5d339b4059bc8c3f7b0e9e366d2eef307c58
Emily Gordon <egordon@pivotal.io>
2017-03-21 12:09:56 -0700

    calculator project

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,137 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Pivotal on 3/20/17.
+//  Copyright © 2017 Emily. All rights reserved.
+//
+
+import Foundation
+
+func factorial(_ number: Double) -> Double {
+    var number = number
+    var result = number
+    while number != 1 {
+        result = result * (number - 1)
+        number = number - 1
+    }
+    return result
+}
+
+func square(_ base: Double) -> Double {
+    return pow(base, 2)
+}
+
+func changeSign(_ number: Double) -> Double {
+    return -number
+}
+
+struct CalculatorBrain {
+    
+    private var accumulator: Double?
+    
+    private var isPending = true
+    
+    private var operationString = " "
+    
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double,Double) -> Double)
+        case equals
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        "π": Operation.constant(Double.pi),
+        "e": Operation.constant(M_E),
+        "√": Operation.unaryOperation(sqrt),
+        "cos": Operation.unaryOperation(cos),
+        "sin": Operation.unaryOperation(sin),
+        "±": Operation.unaryOperation(changeSign),
+        "abs": Operation.unaryOperation(abs),
+        "!" : Operation.unaryOperation(factorial),
+        "²": Operation.unaryOperation(square),
+        "x": Operation.binaryOperation({ $0 * $1 }),
+        "/": Operation.binaryOperation({ $0 / $1 }),
+        "+": Operation.binaryOperation({ $0 + $1 }),
+        "-": Operation.binaryOperation({ $0 - $1 }),
+        "^": Operation.binaryOperation({ pow($0, $1) }),
+        "=": Operation.equals
+    ]
+    
+    mutating func performOperation(_ symbol: String){
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+                buildOperationString(String(value))
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                    buildOperationString(symbol)
+                    isPending = false
+                }
+            case .binaryOperation(let function):
+                if accumulator != nil {
+                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    accumulator = nil
+                    buildOperationString(symbol)
+                    isPending = true
+                }
+            case .equals:
+                performPendingBinaryOperation()
+                isPending = false
+            }
+        }
+    }
+    
+    private mutating func performPendingBinaryOperation() {
+        if pendingBinaryOperation != nil && accumulator != nil {
+            accumulator =  pendingBinaryOperation!.perform(with: accumulator!)
+        }
+       
+    }
+    
+    private var pendingBinaryOperation: PendingBinaryOperation?
+    
+    private struct PendingBinaryOperation {
+        let function: (Double, Double) -> Double
+        let firstOperand: Double
+        func perform(with secondOperand: Double) -> Double {
+            return function(firstOperand, secondOperand)
+        }
+    }
+    
+    mutating func buildOperationString(_ addition: String){
+        operationString = "\(operationString) \(addition)"
+
+    }
+    
+    mutating func setOperand(_ operand: Double) {
+        accumulator = operand
+        if resultIsPending {
+            buildOperationString(String(operand))
+        } else {
+            operationString = String(operand)
+        }
+        
+    }
+    
+    var result: Double? {
+        get {
+            return accumulator
+        }
+    }
+    
+    var resultIsPending: Bool {
+        get {
+            return isPending
+        }
+    }
+    
+    var description: String {
+        get {
+            return operationString
+        }
+    }
+}
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Pivotal on 3/17/17.
+//  Copyright © 2017 Emily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var displayOperation: UILabel!
+    
+    var isTyping = false
+
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if isTyping {
+            let textCurrentlyInDisplay = display.text!
+            if (digit != "."  || !textCurrentlyInDisplay.contains(".")) {
+                display.text = textCurrentlyInDisplay + digit
+            }
+        } else {
+            display.text = digit
+            isTyping = true
+        }
+    }
+    var displayValue: Double {
+        get {
+            if let digit = Double(display.text!) {
+                return digit
+            } else {
+                return 0
+            }
+            
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func updateDisplayOperation(_ sender: UIButton) {
+        var suffix = " ="
+        if (brain.resultIsPending) {
+            suffix = " ..."
+        }
+        displayOperation.text = brain.description + suffix
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if isTyping {
+            brain.setOperand(displayValue)
+            isTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        if let result = brain.result {
+            displayValue = result
+        }
+    }
+    @IBAction func clear(_ sender: UIButton) {
+        brain = CalculatorBrain()
+        display.text = "0"
+        displayOperation.text = " "
+    }
+}
+
